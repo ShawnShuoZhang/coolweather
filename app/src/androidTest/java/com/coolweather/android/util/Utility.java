5e8460686587d4c4b1f8ca3b6d75929b5afba092
ShawnShuoZhang <106317713+ShawnShuoZhang@users.noreply.github.com>
2022-05-27 17:25:16 +0800

    second finish

--- app/src/androidTest/java/com/coolweather/android/util/Utility.java
+++ app/src/androidTest/java/com/coolweather/android/util/Utility.java
@@ -2,6 +2,7 @@ package com.coolweather.android.util;
 
 import android.text.TextUtils;
 
+import com.coolweather.android.db.City;
 import com.coolweather.android.db.Province;
 
 import org.json.JSONArray;
@@ -12,9 +13,8 @@ public class Utility {
     /*
     解析和处理服务器返回的省级数据
      */
-    public static boolean handleProvinceResponse(String response){
-        if(!TextUtils.isEmpty(response))
-        {
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allProvince = new JSONArray(response);
                 for (int i = 0; i < allProvince.length(); i++) {
@@ -31,7 +31,31 @@ public class Utility {
         }
         return false;
     }
-    /*
-    解析和处理服务器返回的市级数据
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     * @param response
+     * @return boolean
+     */
+    public static boolean handleCityResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCity = new JSONArray(response);
+                for (int i = 0; i < allCity.length(); i++) {
+                    JSONObject cityObject = allCity.getJSONObject(i);
+                    City city = new City();
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setCityName(cityObject.getString("name"));
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     *
      */
 }
