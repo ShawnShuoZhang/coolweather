829f5c94c14757584cbb782f1816489e9d616dc4
Shawn <2211276976@qq.com>
2022-05-27 12:51:27 +0800

    First commit

---
+++ app/src/androidTest/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,37 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    /*
+    解析和处理服务器返回的省级数据
+     */
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response))
+        {
+            try {
+                JSONArray allProvince = new JSONArray(response);
+                for (int i = 0; i < allProvince.length(); i++) {
+                    JSONObject provinceObject = allProvince.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.getProvinceName(provinceObject.getString("name"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /*
+    解析和处理服务器返回的市级数据
+     */
+}
